8e1b7f2e5897beecad2b3f918b706b6a8ff24bd5
Khatorress <khaekaua020922@gmail.com>
2024-11-25 19:29:30 -0300

    Add files via upload

---
+++ 09rent-car.py
@@ -0,0 +1,4 @@
+kmrodados = float(input("quantos KM foram rodados?"))
+dias =  float(input("por quantos dias o carro foi usado?"))
+valor = (60 + dias) + (0.15 + kmrodados)
+print(f"o valor a pagar pelo aluguel do carro é {valor}")
